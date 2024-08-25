8717b6105de2a9ae1447c128c964da2bf012a36d
Naoki Takahashi <naoki.tkb@gmail.com>
2024-08-25 17:43:40 +0900

    update

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -4,16 +4,29 @@
 # settings we’re about to change
 osascript -e 'tell application "System Preferences" to quit'
 
+# Dock
+defaults write com.apple.dock autohide -bool "true"
+defaults write com.apple.dock show-recents -bool "false"
+defaults write com.apple.dock tilesize -int "40"
+
 # screenshots
 defaults write com.apple.screencapture location -string "${HOME}/Desktop/screenshot"
 defaults write com.apple.screencapture name "ScreenShot"
-defaults write com.apple.screencapture disable-shadow -bool true
+defaults write com.apple.screencapture disable-shadow -bool "true"
+defaults write com.apple.screencapture show-thumbnail -bool "true"
 
 # Finder
-defaults write com.apple.Finder FXPreferredViewStyle Nlsv
-defaults write com.apple.finder AppleShowAllFiles -bool YES
-defaults write -g AppleShowAllExtensions -bool true
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+# Show hidden files in Finder
+defaults write com.apple.finder AppleShowAllFiles -bool "true"
+# Show files with all extensions
+defaults write -g AppleShowAllExtensions -bool "true"
+# Show path bar in Finder
 defaults write com.apple.finder ShowPathbar -bool "true"
+
+# Keyboard
+defaults write com.apple.inputmethod.Kotoeri JIMPrefLiveConversionKey -bool "false"
+defaults write com.apple.HIToolbox AppleFnUsageType -int "0"
 
 # .DS_Store
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool "true"
